7542b14708636183dc95931053e1cf23b508db30
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-03-19 19:56:25 +0200

    Hello mates was added

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Good evening! We are from Ukraine =)");
+        System.out.println("Hello, mates!");
     }
 }
